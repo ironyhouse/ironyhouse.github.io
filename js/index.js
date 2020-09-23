fe2177fac87a583baae334f749cefee0975dd3ad
ironyhouse <ironylait@gmail.com>
2020-09-23 16:42:04 +0300

    feat: add portfolio check

--- js/index.js
+++ js/index.js
@@ -9,6 +9,8 @@ for (let i = 0; i <= 40; i++) {						// arrLength
 	document.getElementById('matrix').appendChild(document.createElement('b')).innerHTML = numb;  //addStr
 }
 
+
+
 // Up Button
 window.onload = function(){
 	//window.scrollTo(x,y)
@@ -34,6 +36,7 @@ window.onload = function(){
 }
 
 
+
 // Show Elements
 function showElement(id) {    
     if (   document.getElementById(`${id}`).nextElementSibling.style.display == '' 
@@ -48,9 +51,25 @@ function showElement(id) {
 
 
 
+// Portfolio check
+function showPortfolioElements(classElement) {
+	for (let i = 0; i < document.getElementsByClassName('portfolio_check')[0].querySelectorAll('div').length; i++) {
+		document.getElementsByClassName('portfolio_check')[0].querySelectorAll('div')[i].classList.remove('text_check');
+	}
+	document.getElementById(classElement).classList.add('text_check');
+
+
+	for (let i = 0; i < document.getElementsByClassName('portfolio_items').length; i++) {
+		document.getElementsByClassName('portfolio_items')[i].style.display = 'none';
+	}
+	for (let i = 0; i < document.getElementsByClassName(classElement).length; i++) {
+		document.getElementsByClassName(classElement)[i].style.display = 'flex';
+	}
+}
+
 // Portfolio matrix
-document.querySelector(".portfolio_container--item_1").addEventListener("mousemove", function(e) {
-	const skewItem = document.querySelector(".portfolio_container--item_1").getBoundingClientRect();
+document.querySelector('.portfolio_container--item_1').addEventListener("mousemove", function(e) {
+	const skewItem = document.querySelector('.portfolio_container--item_1').getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
 
@@ -59,76 +78,61 @@ document.querySelector(".portfolio_container--item_1").addEventListener("mousemo
 
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
-	document.querySelector(".portfolio_container--item_1").style.setProperty("--x-translate", `${xCalc}`);
-	document.querySelector(".portfolio_container--item_1").style.setProperty("--y-translate", `${yCalc}`);
-});
 
+
+	document.querySelector('.portfolio_container--item_1').style.setProperty("--x-translate", `${xCalc}`);
+	document.querySelector('.portfolio_container--item_1').style.setProperty("--y-translate", `${yCalc}`);
+});
 document.querySelector(".portfolio_container--item_2").addEventListener("mousemove", function(e) {
 	const skewItem = document.querySelector(".portfolio_container--item_2").getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
-
 	const clientX = e.clientX;
 	const clientY = e.clientY;
-
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
-
-
 	document.querySelector(".portfolio_container--item_2").style.setProperty("--x-translate", `${xCalc}`);
 	document.querySelector(".portfolio_container--item_2").style.setProperty("--y-translate", `${yCalc}`);
 });
-
 document.querySelector(".portfolio_container--item_3").addEventListener("mousemove", function(e) {
 	const skewItem = document.querySelector(".portfolio_container--item_3").getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
-
 	const clientX = e.clientX;
 	const clientY = e.clientY;
-
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
 	document.querySelector(".portfolio_container--item_3").style.setProperty("--x-translate", `${xCalc}`);
 	document.querySelector(".portfolio_container--item_3").style.setProperty("--y-translate", `${yCalc}`);
 });
-
 document.querySelector(".portfolio_container--item_4").addEventListener("mousemove", function(e) {
 	const skewItem = document.querySelector(".portfolio_container--item_4").getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
-
 	const clientX = e.clientX;
 	const clientY = e.clientY;
-
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
 	document.querySelector(".portfolio_container--item_4").style.setProperty("--x-translate", `${xCalc}`);
 	document.querySelector(".portfolio_container--item_4").style.setProperty("--y-translate", `${yCalc}`);
 });
-
 document.querySelector(".portfolio_container--item_5").addEventListener("mousemove", function(e) {
 	const skewItem = document.querySelector(".portfolio_container--item_5").getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
-
 	const clientX = e.clientX;
 	const clientY = e.clientY;
-
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
 	document.querySelector(".portfolio_container--item_5").style.setProperty("--x-translate", `${xCalc}`);
 	document.querySelector(".portfolio_container--item_5").style.setProperty("--y-translate", `${yCalc}`);
 });
-
 document.querySelector(".portfolio_container--item_6").addEventListener("mousemove", function(e) {
 	const skewItem = document.querySelector(".portfolio_container--item_6").getBoundingClientRect();
 	const imageCenterX = skewItem.left + skewItem.width / 2;
 	const imageCenterY = skewItem.top + skewItem.height / 2;
-
 	const clientX = e.clientX;
 	const clientY = e.clientY;
-
 	const xCalc = (clientX - imageCenterX) * 0.0000015;
 	const yCalc = (clientY - imageCenterY) * 0.0000015;
 	document.querySelector(".portfolio_container--item_6").style.setProperty("--x-translate", `${xCalc}`);
